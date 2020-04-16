d9b7c0f6e8330fea48272d96c5695b3152439a04
Antonio Ganci <antonio.ganci@leevia.com>
2020-04-16 10:09:09 +0200

    fix error

--- javascript/register_participant.js
+++ javascript/register_participant.js
@@ -1,10 +1,12 @@
+https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+
 var CAMPAIGN_ID = 999 // TODO: replace 999 with your campaign id
 var API_KEY = 'xxx'; // TODO: replace xxx with your api key
 var API_SECRET = 'yyy'; // TODO: replace yyy with your api secret
-var END_POINT = "https://app.leevia.com/api/v1/campaigns" + CAMPAIGN_ID + "/authenticate";
+var END_POINT = "http://app.lvh.me:3000/" + CAMPAIGN_ID + "/authenticate";
 
 timestamp = + new Date();
-message = KEY + "." + timestamp;
+message = API_KEY + "." + timestamp;
 signature = CryptoJS.HmacSHA256(message, API_SECRET).toString(CryptoJS.enc.Hex);
 
 var request = new XMLHttpRequest();
