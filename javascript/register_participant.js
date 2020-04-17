5cf72d1858deeb953c719c618b892d4c0682dede
Antonio Ganci <antonio.ganci@leevia.com>
2020-04-17 11:07:39 +0200

    add participant creation on javascript sample

--- javascript/register_participant.js
+++ javascript/register_participant.js
@@ -26,7 +26,9 @@ const options = {
 
 const req = https.request(options, (res) => {
   console.log(`STATUS: ${res.statusCode}`);
+  var authorization = res.headers['authorization'];
   console.log(`HEADERS: ${res.headers['authorization']}`);
+  do_participant_request(authorization)
 });
 
 req.on('error', (e) => {
@@ -34,3 +36,45 @@ req.on('error', (e) => {
 });
 
 req.end();
+
+function do_participant_request(authorization) {
+  var participant_data = {
+    first_name: 'Mario',
+    last_name: 'Rossi',
+    email: 'mario.rossi@example.com',
+    registration_ip: '192.168.0.4',
+    custom_data: {
+      date_of_birth: '1989-10-03'
+    },
+    acceptances: {
+      rules: true,
+      newsletter: false
+    }
+  };
+
+  var participant_data_string = JSON.stringify(participant_data)
+
+  const options = {
+  hostname: HOST,
+  path: '/api/v1/campaigns/instant_wins/' + CAMPAIGN_ID + '/participants',
+  method: 'POST',
+  headers: {
+    "Accept": 'application/vnd.leevia.api.v1+json',
+    'Content-Type': 'application/json',
+    'Content-Length': participant_data_string.length,
+    "Authorization": authorization
+    }
+  };
+  const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+  });
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  req.write(participant_data_string);
+  req.end();
+}
